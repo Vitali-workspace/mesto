3d0d6de24b8fb6c469d9b58c924f418c49cd2476
Vitali_Slepnyov <icofundss@gmail.com>
2021-08-15 19:43:03 +0300

    fix: исправление попапа

--- script/script.js
+++ script/script.js
@@ -7,13 +7,14 @@ let buttonSave = container.querySelector('.popup__btn-save');
 
 function showClick() {
 messageEdit.classList.add('popup_opened');
-messageEdit.classList.remove('popup__hidden');
-popupBackground.classList.add('popup_opened');
+messageEdit.classList.remove('root__hidden');
+popupBackground.classList.remove('root__hidden');
 }
 
 function сloseClick() {
     popupBackground.classList.remove('popup_opened');
-    messageEdit.classList.add('popup__hidden');
+    messageEdit.classList.add('root__hidden');
+    popupBackground.classList.add('root__hidden');
     formElement.reset();
 }
 
@@ -32,7 +33,7 @@ function formSubmitHandler (evt) {
     const jobsNew = nameInput.textContent;
     jobInput.textContent = `${jobs}`;
     popupBackground.classList.remove('popup_opened');
-    messageEdit.classList.add('popup__hidden');
+    popupBackground.classList.add('root__hidden');
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
