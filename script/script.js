274306154ba9438feb77111f66c47aca10e231b6
Vitali <81440120+Vitali-workspace@users.noreply.github.com>
2022-02-13 20:59:05 +0300

    update file

--- script/script.js
+++ script/script.js
@@ -2,29 +2,35 @@ let container = document.querySelector('.root');
 let profile = container.querySelector('.profile');
 let popup = container.querySelector('.popup');
 let btnClose = container.querySelector('.popup__btn-close');
-let btnSave = container.querySelector('.popup__btn-save');
+let formEdit = container.querySelector('#formEdit');
+
 let btnProfileEdit = container.querySelector('.profile__btn-edit');
 // обращение к тексту в профиле
 let profileName = profile.querySelector('.profile__name');
 let profileDescription = profile.querySelector('.profile__description');
-// обращение к инпута
+// обращение к инпутам
 let inputName = container.querySelector('#inputEditName');
 let inputDescription = container.querySelector('#inputEditText');
 
-btnProfileEdit.addEventListener('click', function () {
-  popup.classList.toggle('popup_opened');
-  inputName.value = profileName.innerHTML;
-  inputDescription.value = profileDescription.innerHTML;
-});
+function openPopup() {
+  inputName.value = profileName.textContent;
+  inputDescription.value = profileDescription.textContent;
+  popupClose()
+}
 
-btnClose.addEventListener('click', function () {
+function popupClose() {
   popup.classList.toggle('popup_opened');
-});
+}
 
-btnSave.addEventListener('click', function (evt) {
+function submitForm(evt) {
   evt.preventDefault();
   // вставка значения из инпута в профиль
-  profileName.textContent = `${inputName.value}`;
-  profileDescription.textContent = `${inputDescription.value}`;
-  popup.classList.remove('popup_opened');
-});
+  profileName.textContent = inputName.value;
+  profileDescription.textContent = inputDescription.value;
+  popupClose()
+}
+
+btnProfileEdit.addEventListener('click', openPopup);
+btnClose.addEventListener('click', popupClose);
+formEdit.addEventListener('submit', submitForm);
+
